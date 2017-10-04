9f754919b9ca9ee64ede88db80dd8d66d740d3e8
Chris Lesesne <clesesne3@gmail.com>
2017-10-03 20:02:08 -0700

    question 7 to a function

--- app.js
+++ app.js
@@ -104,48 +104,51 @@ function funcQuestion6() {
 }
 funcQuestion6();
 
+function funcQuestion7() {
 // Multiple correct guessible answers from an array, six tries to get a correct answer.
-var subjects = ['English', 'jogging', 'geology', 'Spanish'];
-var subjectTest = subjects;
-var subjectString = '';
-// Concatinate Subjects into a single string for printing inline in an alert.
-for (i = 0; i < subjects.length; i++){
-  subjectString = subjectString + subjects[i] + ', ';
-  if (i == subjects.length - 2)
-    subjectString = subjectString + ' and ';
-}
-for (var i = 0; i < 6; i++){
-  console.log('Before prompt, i = ' + i);
-  var answer7 = prompt('Besides computer coding what other subjects did Arthur study at WSU?', 'Enter an academic subject.');
-  //Loop through each element of subjects to find a match
-  var found = false;
-  for (var j = 0; j < subjectTest.length; j++){
-    console.log('Top of j loop. j, subjects[j], answer7 ' + j + ' ' + subjects[j] + ' ' + answer7);
-    if (subjects[j] == answer7){
-      found = true;
-      var removedItem = subjectTest.splice(j,1); // If item is removed before j loop, it will reference an undefined index
-      console.log('After if (subjects[j] == answer7). found set to true. j, subjects[j], answer7 = ' + j + ' ' + subjects[j] + ' ' + answer7);
-      break;
-    }//if subjects[j] == answer7
-  }// next j
-  console.log('Before if found test, found, j ' + found + ' ' + j);
-  if (found){
-    alert('Correct! ' + answer7 + ' Was one of Arthur\'s favorite subjects.  There are ' + (subjectTest.length) + ' more subjects.');
-    i--; //Decrement i so it doesn't count against a wrong answer.
-    console.log('found == true. i decremented. Answer7, i & j: ' + answer7 + ' ' + i + ' ' + j);
-  }else{
-    console.log('found != true. Answer7, subjects[j], i & j: ' + answer7 + ' ' + subjects[j] + ' ' + i + ' ' + j);
-    //To avoid telling the user "You have 0 more chances," then ending the round:
-    if (i < 5){
-      alert('Sorry, that\'s not right. You have ' + (5 - i) + ' more chances.');
-    }else{
-      alert('Sorry, Arthur took ' + subjectString + 'while at Washington State University.');
-    } //if i < 5
-  } //If found
-  if (subjectTest.length < 1){
-    userPoints++;
-    alert('You won the game!  Arthur took ' + subjectString + 'while at Washington State University.');
-    alert(user + ' you have ' + userPoints + ' out of 7 points.');
-    break;
+  var subjects = ['English', 'jogging', 'geology', 'Spanish'];
+  var subjectTest = subjects;
+  var subjectString = '';
+  // Concatinate Subjects into a single string for printing inline in an alert.
+  for (i = 0; i < subjects.length; i++){
+    subjectString = subjectString + subjects[i] + ', ';
+    if (i == subjects.length - 2)
+      subjectString = subjectString + ' and ';
   }
-} //next i
+  for (var i = 0; i < 6; i++){
+    console.log('Before prompt, i = ' + i);
+    var answer7 = prompt('Besides computer coding what other subjects did Arthur study at WSU?', 'Enter an academic subject.');
+    //Loop through each element of subjects to find a match
+    var found = false;
+    for (var j = 0; j < subjectTest.length; j++){
+      console.log('Top of j loop. j, subjects[j], answer7 ' + j + ' ' + subjects[j] + ' ' + answer7);
+      if (subjects[j] == answer7){
+        found = true;
+        var removedItem = subjectTest.splice(j,1); // If item is removed before j loop, it will reference an undefined index
+        console.log('After if (subjects[j] == answer7). found set to true. j, subjects[j], answer7 = ' + j + ' ' + subjects[j] + ' ' + answer7);
+        break;
+      }//if subjects[j] == answer7
+    }// next j
+    console.log('Before if found test, found, j ' + found + ' ' + j);
+    if (found){
+      alert('Correct! ' + answer7 + ' Was one of Arthur\'s favorite subjects.  There are ' + (subjectTest.length) + ' more subjects.');
+      i--; //Decrement i so it doesn't count against a wrong answer.
+      console.log('found == true. i decremented. Answer7, i & j: ' + answer7 + ' ' + i + ' ' + j);
+    }else{
+      console.log('found != true. Answer7, subjects[j], i & j: ' + answer7 + ' ' + subjects[j] + ' ' + i + ' ' + j);
+      //To avoid telling the user "You have 0 more chances," then ending the round:
+      if (i < 5){
+        alert('Sorry, that\'s not right. You have ' + (5 - i) + ' more chances.');
+      }else{
+        alert('Sorry, Arthur took ' + subjectString + 'while at Washington State University.');
+      } //if i < 5
+    } //If found
+    if (subjectTest.length < 1){
+      userPoints++;
+      alert('You won the game!  Arthur took ' + subjectString + 'while at Washington State University.');
+      alert(user + ' you have ' + userPoints + ' out of 7 points.');
+      break;
+    }
+  } //next i
+}
+funcQuestion7();
